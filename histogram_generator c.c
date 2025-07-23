688daa13dd6a7b3fee73e0bda3ce54ad68853bc6
Lester Sotolongo <66220446+lesotolongo@users.noreply.github.com>
2025-07-22 21:51:56 -0400

    Delete histogram_generator c.c

--- histogram_generator c.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-// Function prototype
-void generate_histogram_placeholder();
-
-// A placeholder function for histogram generation
-void generate_histogram_placeholder() {
-    printf("Histogram generator is not yet implemented.\n");
-}
